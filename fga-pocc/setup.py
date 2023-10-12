bd25dcd50cc20280c4de45e4009314d3fe8c5f65
Clifford Frempong <41656028+CliffLolo@users.noreply.github.com>
2023-10-12 23:50:48 +0000

    Update setup.py

--- fga-pocc/setup.py
+++ fga-pocc/setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='fga-poc',
-    version='0.1.0',
+    name='{{ cookiecutter.pypi_name }}',
+    version='{{ cookiecutter.version }}',
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}',
+    license='MIT',
+    author='{{ cookiecutter.full_name }}',
+    description='{{ cookiecutter.project_short_description }}',
+    long_description=__doc__,
+    # name='fga-poc',
+    # version='0.1.0',
     packages=find_packages(),
     install_requires=[
         'typer',
